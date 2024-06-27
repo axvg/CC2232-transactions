1f4970b9ea39d9a291a6ab0992c618701128ddf3
axvg <axvg@users.noreply.github.com>
2024-06-26 19:32:51 -0500

    fix parsing, execution

--- src/main/java/com/example/DBTransactionManager.java
+++ src/main/java/com/example/DBTransactionManager.java
@@ -1,5 +1,6 @@
 package com.example;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -55,157 +56,118 @@ public class DBTransactionManager {
         }
     
         // Lee el archivo de instrucciones y ejecuta los comandos
-        void getInstructions(String fileName) {
-            File file = new File(fileName);
-            Scanner scanner;
+        public void getInstructions(String filePath) {
             try {
-                scanner = new Scanner(file);
-                commandCount = 0;
-        
+                File file = new File(filePath);
+                Scanner scanner = new Scanner(file);
+                List<String> instructionsList = new ArrayList<>();
                 while (scanner.hasNextLine()) {
-                    scanner.nextLine();
-                    commandCount++;
+                    String instruction = scanner.nextLine();
+                    instructionsList.add(instruction);
                 }
-        
-        
-                commands = new String[commandCount];
-                suspended = new String[commandCount];
-        
-                scanner = new Scanner(file);
-                int i = 0;
-        
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    // System.out.println("line " + line);
-                    commands[i] = line;
-                    i++;
-                }
+                scanner.close();
+                commands = new String[instructionsList.size()];
+                suspended = new String[instructionsList.size()];
+                commandCount = instructionsList.size();
+                instructionsList.toArray(commands);
                 executeCommands(commands, commandCount);
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage() + " " + e.getCause());
-                e.printStackTrace();
-                throw new RuntimeException("Error reading file");
+            } catch (FileNotFoundException e) {
+                System.err.println("Error al leer archivo: " + e.getMessage());
             }
         }
-    
+
         private void resetSuspendedCount() {
             suspendedCount = 0;
         }
 
         public void executeCommands(String[] commands, int commandCount) {
             resetSuspendedCount();
+            for (int i = 0; i < commandCount; i++) {
+                String instruction = commands[i];
 
-            for (String command : commands) {
-                if (command == null || command.isEmpty()) {
-                    continue;
-                }
-                
-                // Esto se usa para saber si se ha modificado el grafo en cada loop
+                // Para saber si el grafo ha sido modificado
                 modified = false;
-
+                
                 // Obtiene el codigo de estado de command (0, 1, 2)
-                int response = parseInstruction(command);
-
-                // Si se ha modificado el grafo, se reorganiza el array de comandos
-                if (modified){
-                    for (int i = 0; i < commandCount - 1; i++){
-                            commands[i] = commands[i + 1];
-                        }
-                        commands[commandCount - 1] = command;
+                int response = parseInstruction(instruction);
+                if (modified) {
+                    // Si el grafo se ha modificado
+                    // Se reorganiza el array de comandos
+                    // Se elimina el comando actual y se agrega al final
+                    for (int j = i; j < commandCount - 1; j++) {
+                        commands[j] = commands[j + 1];
+                    }
+                    commands[commandCount - 1] = instruction;
                 } else {
-                    // Si no se ha modificado el grafo, se imprime el mensaje correspondiente
-                    if (response == 0){
-                        System.out.println("Transaction " + command + " is finished");
-                    }
-
-                    if (response == 1){
-                        System.out.println("Transaction " + command + " is suspended");
-                        suspended[suspendedCount] = command;
-                        suspendedCount++;
-                    }
-
-                    if (response == 2){
-                        for (int i = 0; i < suspendedCount - 1; i++){
-                            // System.out.println("Suspended number: " + suspendedCount);
-                            // System.out.println("??? " + suspended[i]);
-                        }
-                        // En este caso (2), se ejecutan los comandos suspendidos
+                    // Si no ha sido modificado
+                    // Se imprime el mensaje correspondiente
+                    if (response == 0) {
+                        System.out.println(instruction + " finished");
+                    } else if (response == 1) {
+                        System.out.println(instruction + " suspended");
+                        suspended[suspendedCount++] = instruction;
+                    } else {
                         executeCommands(suspended, suspendedCount);
                     }
                 }
             }
         }
+
     
         // Parsea la instruccion y ejecuta la instruccion correspondiente
         // La entrada tiene esta forma: read(T1, A3) o end(T2)
         // El formato es: <instruccion>(<transaction>, <data>)
         private int parseInstruction(String instruction) {
-            int openBracketIdx = instruction.indexOf('(');
-            int closeBracketIdx = instruction.indexOf(')');
-            String instructionType = instruction.substring(0, openBracketIdx);
-            String content = instruction.substring(openBracketIdx + 1, closeBracketIdx);
-
-            // System.out.println("Instruction Type: " + instructionType + " Content: " + content + " Instruction: " + instruction);
-
+            int openBracketIndex = instruction.indexOf('(');
+            String instructionType = instruction.substring(0, openBracketIndex);
+            instruction = instruction.substring(openBracketIndex + 1);
+    
             String data;
             String transaction;
 
             // Si la instruccion es end, no hay data porque es el final de la transaccion
             if (instructionType.equals("end")) {
+                transaction = instruction.substring(0, instruction.indexOf(')'));
                 data = "";
-                transaction = content;
             } else {
                 // Si la instruccion no es end, se obtiene la data y la transaccion
-                String[] dataTransaction = getDataTransaction(content);
-                data = dataTransaction[0];
-                transaction = dataTransaction[1];
+                int commaIndex = instruction.indexOf(',');
+                transaction = instruction.substring(0, commaIndex);
+                instruction = instruction.substring(commaIndex + 1);
+                int closedBracketIndex = instruction.indexOf(')');
+                data = instruction.substring(0, closedBracketIndex);
             }
-
+    
             // System.out.println("Data: " + data + " Transaction: " + transaction);
 
-            Data findData = addData(data);
-            Transaction findTransaction = addTransaction(transaction);
-
+            Data dataAdded = addData(data);
+            Transaction addedTransaction = addTransaction(transaction);
+            
             // Ejecuta la instruccion con la transaccion y la data
-            return execute(instructionType, findTransaction, findData);
+            return execute(instructionType, addedTransaction, dataAdded);
         }
 
-        private String[] getDataTransaction(String content) {
-            String[] split = content.split(",");
-            return split;
-        }
     
-        private int execute(String instructionType, Transaction findTransaction, Data findData) {
-
-            // Para cada transaccion, se inserta un nodo en el grafo
-            waitForGraph.insertNode(findTransaction);
-
-            // El estado inicial es 0
+        private int execute(String instructionType, Transaction targetTransaction, Data findData) {
+            waitForGraph.insertNode(targetTransaction);
             int executionStatus = 0;
-
-            // Si la instruccion no es 'end'
-            if (!instructionType.equals("end")){
-                if (findData.isLocked() && findData.lockTransaction() != findTransaction.getName()){
+            if (!instructionType.equals("end")) {
+                if (findData.isLocked() && !findData.lockTransaction().equals(targetTransaction.getName())) {
                     executionStatus = 1;
-
-                    // Se agrega una arista al grafo
+                    targetTransaction.suspend();
                     Transaction waitFor = addTransaction(findData.lockTransaction());
-                    waitForGraph.insertEdge(waitFor, findTransaction, findData);
+                    waitForGraph.insertEdge(waitFor, targetTransaction, findData);
                 } else {
                     executionStatus = 0;
-                    // Si la instruccion es 'read', se bloquea la data
-                    findData.lockData(findTransaction.getName());
+                    findData.lockData(targetTransaction.getName());
                 }
             } else {
-                    // Si la instruccion es 'end' se borra el nodo
-                waitForGraph.deleteNode(findTransaction);
+                waitForGraph.deleteNode(targetTransaction);
                 executionStatus = 2;
             }
-
-            if (executionStatus != 0){
+            if (executionStatus != 0) {
                 Transaction toDelete = waitForGraph.findMostRecentTransaction();
-
-                if (toDelete != null){
+                if (toDelete != null) {
                     waitForGraph.deleteNode(toDelete);
                     modified = true;
                 }
